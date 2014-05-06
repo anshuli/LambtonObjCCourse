7fec0786ea80adbdd97510cc1067c74a7b6323db
Ryan Laudi <ryanlaudi@rogers.com>
2014-05-06 13:54:04 -0400

    Starter Project
    Hello World Exxample intro to objects

---
+++ day2CodeExample/day2CodeExample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  day2CodeExample
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
