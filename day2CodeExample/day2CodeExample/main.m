afe95b576a7eba011a7b1747aaa34f606e30938b
anshuli <anshuli.ch@gmail.com>
2014-05-06 17:09:34 -0400

    XYPoint
    cordinates

--- day2CodeExample/day2CodeExample/main.m
+++ day2CodeExample/day2CodeExample/main.m
@@ -7,16 +7,65 @@
 //
 
 #import <Foundation/Foundation.h>
+//@interface section
+@interface XYPoint : NSObject
+-(void)print;
+-(void)setXCordinator: (int)x;
+-(int)getXCordinator;
+-(void)setYCordinator: (int)y;
+-(int)getYCordinator;
+@end
 
+// @implementation section
+@implementation XYPoint
+{
+    int XCordinator;
+    int YCordinator;
+}
+-(void)print
+{
+    NSLog(@"%i/%i",XCordinator,YCordinator);
+
+}
+-(void)setXCordinator:(int)x
+{
+    XCordinator=x;
+}
+
+-(int)getXCordinator
+{
+    return XCordinator;
+}
+
+-(void)setYCordinator:(int)y
+{
+    YCordinator=y;
+}
+
+-(int)getYCordinator
+{
+    return YCordinator;
+}
+
+@end
+// program section
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        int numerator=1;
-        int denominator=3;
-        NSLog(@"the fraction is %i/%i ",numerator,denominator);
+        XYPoint *myXYPoint;
+        //create  an instance of fraction
+        myXYPoint = [XYPoint alloc];
+        myXYPoint = [myXYPoint init];
         
+        //set the fraction
+        [myXYPoint setXCordinator:1];
+        [myXYPoint setYCordinator:3];
+        
+        //display the fraction using print method
+        NSLog(@"the value of myXYPoint is:");
+        [myXYPoint print];
     }
     return 0;
 }
