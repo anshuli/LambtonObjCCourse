f50f7cd46280e8bbbba835974dc507ac8a636bcb
anshuli <anshuli.ch@gmail.com>
2014-05-13 14:13:26 -0400

    primeNumbers

--- day7/day7prime/day7prime/main.m
+++ day7/day7prime/day7prime/main.m
@@ -13,10 +13,33 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int p, d;
+        BOOL isPrime;
         
+        for(p=2 ; p<=50; ++p)
+        {
+            isPrime = YES;
+            
+            for (d=2; d<p; ++d)
+            {
+                if(p%d==0)
+                {
+                    isPrime=NO;
+                }
+            
+                
+            }
+        
+        
+        if (isPrime == YES)
+        {
+        
+            NSLog(@"%i",p);
+        }
+        
+        }
     }
+ 
     return 0;
 }
 
